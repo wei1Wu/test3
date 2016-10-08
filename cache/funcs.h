1fbec6662fb2f19630d41328fb178a3e1bc3c805
davissonwu <1174846945@qq.com>
2016-10-08 23:19:16 +0800

    修改了这个submodule，因为他的文件和猪工程的冲突了

---
+++ cache/funcs.h
@@ -0,0 +1,13 @@
+//
+//  func.h
+//  multiLanguage
+//
+//  Created by davissonwu on 16/10/8.
+//  Copyright © 2016年 davissonwu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface funcs : NSObject
+- (void) printhaha;
+@end
---
+++ cache/funcs.m
@@ -0,0 +1,15 @@
+//
+//  func.m
+//  multiLanguage
+//
+//  Created by davissonwu on 16/10/8.
+//  Copyright © 2016年 davissonwu. All rights reserved.
+//
+
+#import "funcs.h"
+
+@implementation funcs
+-(void)printhaha{
+    NSLog(@"给大爷哈一个，😄");
+}
+@end
